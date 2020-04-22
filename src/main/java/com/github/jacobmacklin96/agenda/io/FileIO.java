7460065c1efb6e3a20f003c3d6fce6b105f38130
Jacob Macklin <bigmack81@gmail.com>
2020-04-22 15:20:56 -0700

    Parsed and read from file

--- src/main/java/com/github/jacobmacklin96/agenda/io/FileIO.java
+++ src/main/java/com/github/jacobmacklin96/agenda/io/FileIO.java
@@ -36,17 +36,30 @@ public class FileIO {
             String line = reader.readLine();
             String[] outputs = new String[3];
 
-            String[] s = new String[2];
-            s = line.split("/");
-            s[0].replaceAll(" ", "0");
-            outputs[0] = s[0].substring(s[0].length()-2, s[0].length());
-            outputs[0] += "/";
-            if(s[1].substring(1,2).equals(" ")) {
-                outputs[0] += "0" + s[1].substring(0,1);
-            } else {
-                outputs[0] += s[1].substring(0,2);
+            String[] s = new String[3];
+            s = line.split(":");
+            outputs[0] = s[1].substring(s[1].length()-2, s[1].length());
+            if(outputs[0].substring(0,1).equals(" ")) {
+                outputs[0] = "0" + outputs[0].substring(1,2);
             }
-            System.out.println(outputs[0]);
+            outputs[0] += ":" + s[2].substring(0,2);
+            
+            s = s[2].split("/");
+            outputs[1] = s[0].substring(s[0].length()-2, s[0].length());
+            if(outputs[1].substring(0,1).equals(" ")) {
+                outputs[1] = "0" + outputs[1].substring(1,2);
+            }
+            outputs[1] += "/";
+            if(s[1].length() == 1) {
+                outputs[1] += "0" + s[1];
+            } else {
+                outputs[1] += s[1];
+            }
+            
+            outputs[2] = reader.readLine();
+
+            Timeslot t = AgendaLogic.presetParse(outputs[1], outputs[0], outputs[2]);
+            AgendaLogic.add(t, agenda);
             
 
         } catch (FileNotFoundException e) {
